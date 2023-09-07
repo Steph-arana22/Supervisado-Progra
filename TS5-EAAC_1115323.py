2cb44539d449a6461ad734ef5bf2c32d45acb259
Steph-arana22 <143531094+Steph-arana22@users.noreply.github.com>
2023-09-07 07:47:57 -0600

    TS5- Estephanie Arana 1115323

---
+++ TS5-EAAC_1115323.py
@@ -0,0 +1,12 @@
+#Introducción a la programación, sección:09"
+#07/09/2023
+#Estephanie Alexandra Arana Corado
+#Practicar el uso de las instrucciones print e input en Python
+
+print ("Mi segundo programa")
+Nombre=input("Por favor, ingrese su nombre")
+Edad=input("Por favor, ingrese su edad")
+Carrera=input("Por favor, ingrese su carrera")
+Carné=input("Por favor, ingrese su carne")
+print("Soy "+ Nombre +", tengo "+ Edad+ " años y estudio la carrera de " +Carrera + ".")
+print("Mi número de carné es, " +Carné+"." )
